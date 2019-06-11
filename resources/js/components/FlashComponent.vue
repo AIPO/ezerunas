f43d150604296e6f0f9a79c7b0cc03dadcea9534
povilas_kiro <povilas@kiro.tech>
2019-06-11 22:34:06 +0300

    written first logican flash component and updated all npm packages

---
+++ resources/js/components/FlashComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="alert alert-warning alert-flash" role="alert">
+    <strong>Success</strong>
+    {{message}}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Temp message"
+    };
+  },
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
+<style>
+.alert-flash {
+  position: fixed;
+  right: 25px;
+  bottom: 20px;
+}
+</style>
